8f1f29c999948442004e4e00f4899fc1ae2e3e52
Maulik Shah <maulik.shah715@gmail.com>
2020-11-28 18:23:55 +0530

    Now the Audiobook is saved in MP3 format also

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import pyttsx3
 import PyPDF2
+from time import sleep
+from gtts import gTTS
+
 book = open('example.pdf', 'rb')
+out = open('output.txt', 'a')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print("The number of pages in the pdf is: {}".format(pages))
-speaker = pyttsx3.init()
+print("Your PDF is being processed...")
 for curr_page in range(0, pages):
     page = pdfReader.getPage(curr_page)
     text = page.extractText()
-    speaker.say(text)
-    speaker.runAndWait()
+    out.write(text)
+out.close()
+
+print("Your Audibook is being created...")
+
+with open("output.txt") as file:
+    file = file.read()
+
+speak = gTTS(file, lang='en')
+speak.save('audiobook.mp3')
+print("Successfully created the Audiobook")
+
+sleep(7)
